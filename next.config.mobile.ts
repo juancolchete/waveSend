4e1d695deac67961d0683c7d67a3fd7dd08ec65f
juancolchete <juanudkp@gmail.com>
2026-05-12 09:21:12 -0300

    fix: configuration files to fix build errors

--- next.config.mobile.ts
+++ next.config.mobile.ts
@@ -3,15 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export",
   images: {
-    unoptimized: true, 
+    unoptimized: true,
   },
   trailingSlash: true,
-  typescript: {
-    ignoreBuildErrors: true, 
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
 };
 
 export default nextConfig;
