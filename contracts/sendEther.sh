6720bcfeff75ad14c80b22dcb5e15b80e35175f7
juancolchete <juanudkp@gmail.com>
2026-05-15 16:55:25 -0300

    chore: pass a big gas limit

--- contracts/sendEther.sh
+++ contracts/sendEther.sh
@@ -1,2 +1,2 @@
 source .env
-cast send $RECIPIENT_ADDRESS --value 10ether --private-key $DEPLOYER_PRIVATE_KEY --rpc-url http://127.0.0.1:8545 
+cast send $RECIPIENT_ADDRESS --value 10ether --private-key $DEPLOYER_PRIVATE_KEY --rpc-url http://127.0.0.1:8545 --gas-limit 5000000 
