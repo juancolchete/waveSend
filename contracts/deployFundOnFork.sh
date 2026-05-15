e5f3c2e440a8822761f5a890a83b228033296b1d
juancolchete <juanudkp@gmail.com>
2026-05-15 17:16:38 -0300

    ci: gives a lot of celo on deploy fund

--- contracts/deployFundOnFork.sh
+++ contracts/deployFundOnFork.sh
@@ -1,3 +1,3 @@
 source .env
-cast rpc anvil_setBalance $DEPLOYER_ADDRESS  $(cast --to-hex $(cast --to-wei 100 ether)) --rpc-url http://localhost:8545
+cast rpc anvil_setBalance $DEPLOYER_ADDRESS  $(cast --to-hex $(cast --to-wei 1000000000 ether)) --rpc-url http://localhost:8545
 forge script DeployWaveSendFund --rpc-url http://127.0.0.1:8545 --broadcast --gas-price 200000000000
