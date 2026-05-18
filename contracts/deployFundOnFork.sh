4ce77362fd4a2f585af032df748ea8a8a0f5a1da
juancolchete <juanudkp@gmail.com>
2026-05-18 20:02:14 -0300

    ci: add local blockchain script on deploy on fork

--- contracts/deployFundOnFork.sh
+++ contracts/deployFundOnFork.sh
@@ -1,3 +1,4 @@
 source .env
+docker compose -f ./local-blockchain.yml -d
 cast rpc anvil_setBalance $DEPLOYER_ADDRESS  $(cast --to-hex $(cast --to-wei 1000000000 ether)) --rpc-url http://localhost:8545
 forge script DeployWaveSendFund --rpc-url http://127.0.0.1:8545 --broadcast --gas-price 200000000000
